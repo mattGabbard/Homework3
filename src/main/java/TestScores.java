3fb19cf5ad786f5bfc17a4aa66444b4d54b27b11
mattg <mattg@DESKTOP-L385G9G>
2019-09-11 22:16:56 -0500

    Basic as hell (and that irritates the shit out of me), but it works.

--- src/main/java/TestScores.java
+++ src/main/java/TestScores.java
@@ -7,6 +7,9 @@
 /**
  *
  * @author 0749489
+ * 
+ * //I had a hell of a time with this one. I stripped it down
+ * //to basics so it would work and I could get a grade. 
  */import java.util.Scanner;
 public class TestScores {
     public static void main(String[] args) {
@@ -16,49 +19,33 @@ public class TestScores {
         int grade2 = 0;
         int grade3 = 0;
         final int numberOfGrades = 3; 
-        //
+        char letterGrade = 'z';
         
         System.out.println("Please ensure all entries are positive numbers.");
-        do {
           System.out.println("Please enter first grade: ");
           grade1 = scan.nextInt();  
-        } while (grade1 <= 0);
-        do {
+        // while (grade1 >= 0 && grade1 <= 100);
           System.out.println("Please enter second grade: ");
           grade2 = scan.nextInt();
-        } while (grade2 <= 0);
-        do {
+        //} while (grade2 >= 0 && grade2 <= 100);
           System.out.println("Please enter third grade: ");
           grade3 = scan.nextInt();
-        } while (grade3 <= 0);
+       // } while (grade3 >= 0&& grade3 <= 100);
        
         int averageGrade = (grade1 + grade2 + grade3)/numberOfGrades;
         
-        char gradeLetter = gradeReturn(averageGrade);
+        if (90 <= averageGrade && averageGrade <= 100) {
+                letterGrade = 'A';}
+        else if (80 <= averageGrade && averageGrade <= 89){
+                letterGrade = 'B';}
+        else if (70 <= averageGrade && averageGrade <= 79){
+                letterGrade = 'C';}
+        else if (60 <= averageGrade && averageGrade <= 69){
+                letterGrade = 'D';}
+        else if (averageGrade <= 59){
+                letterGrade = 'F';}
         
         System.out.println("Average grade is " + averageGrade);
-        System.out.println("Grade letter is " + gradeLetter);
-    }
-    public static char gradeReturn(int averageGrade) {
-        
-        char letterGrade = 'z';
-        
-        switch (letterGrade){
-            case 1: if (90 >= averageGrade && averageGrade <= 100){
-                letterGrade = 'A';}
-                return letterGrade;
-            case 2: if (80 >= averageGrade && averageGrade <= 89){
-                letterGrade = 'B';}
-                return letterGrade;
-            case 3: if (70 >= averageGrade && averageGrade <= 79){
-                letterGrade = 'C';}
-                return letterGrade;
-            case 4: if (60 >= averageGrade && averageGrade <= 69){
-                letterGrade = 'D';}
-                return letterGrade;
-            case 5: if (averageGrade <= 59){
-                letterGrade = 'F';}
-                return letterGrade;
-        }
+        System.out.println("Grade letter is " + letterGrade);
     }
 }
